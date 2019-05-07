67253a565d68ada18786b7d30bca81d9fa319576
Roni Choudhury <roni.choudhury@kitware.com>
2019-05-07 17:09:57 -0400

    Abstract out promisification process into functions

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,34 @@
 import { json } from 'd3-request';
 
+function promisifyGet (url ) {
+    return new Promise((resolve, reject) => {
+        json(url, (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve(data);
+        });
+    });
+}
+
+function promisifyPost (url, postContent) {
+    return new Promise((resolve, reject) => {
+        json(url)
+            .header('Content-Type', 'application/json')
+            .post(JSON.stringify(postContent), (error, data) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(data);
+            });
+
+    });
+}
+
 export async function getNode (nodeId) {
     const query = JSON.stringify({
         query: `query {
@@ -99,176 +128,61 @@ export function getNodeTree(graph, db, root, includeRoot) {
         url += `/${encodeURIComponent(root)}/${includeRoot.toString()}`;
     }
 
-    const body = JSON.stringify({
+    return promisifyPost(url, {
         treeNodes: graph ? graph.nodes.map((n) => n.uuid) : ['']
     });
-
-    const result = new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(body, (error, graph) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(graph);
-            });
-    });
-
-    return result;
 }
 
 export function getNodes(selectedDB, graph, info) {
     console.log('getNodes()');
 
-    const url = `api/data_api/getNodes/${selectedDB}`;
-
-    const postContent = JSON.stringify({
+    return promisifyPost(`api/data_api/getNodes/${selectedDB}`, {
         rootNode: '',
         rootNodes: info.children.map((n) => n.uuid),
         treeNodes: graph.nodes.map((n) => n.uuid)
     });
-
-    const result = new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(postContent, (error, graph) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(graph);
-            });
-    });
-
-    return result;
 }
 
 export function getProperty(db, name, graph) {
     console.log('getProperty()');
 
-    const url = 'api/data_api/property/' + db + '/' + name;
-    const postContent = JSON.stringify({ 'treeNodes': graph ? graph.nodes.map((n) => { return n.uuid; }) : [''] });
-
-    const result = new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(postContent, (error, data) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(data);
-            });
+    return promisifyPost(`api/data_api/property/${db}/${name}`, {
+        treeNodes: graph ? graph.nodes.map((n) => { return n.uuid; }) : ['']
     });
-
-    return result;
 }
 
 export function getProperties(db) {
     console.log('getProperties()');
 
-    const url = `api/data_api/properties/${db}`;
-
-    return new Promise((resolve, reject) => {
-        json(url, (error, data) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-
-            resolve(data);
-        });
-    });
+    return promisifyGet(`api/data_api/properties/${db}`);
 }
 
 export function getLabels(db) {
     console.log('getLabels()');
 
-    const url = `api/data_api/labels/${db}`;
-
-    const result = new Promise((resolve, reject) => {
-        json(url, (error, data) => {
-            if (error) {
-                reject(error);
-                return;
-            }
-
-            resolve(data);
-        });
-    });
-
-    return result;
+    return promisifyGet(`api/data_api/labels/${db}`);
 }
 
 export function getEdges(db, uuid, nodes) {
     console.log('getEdges()');
 
-    const url = `api/data_api/edges/${db}/${encodeURIComponent(uuid)}`;
-
-    const postContent = JSON.stringify({
+    return promisifyPost(`api/data_api/edges/${db}/${encodeURIComponent(uuid)}`, {
         treeNodes: nodes
     });
-
-    const result = new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(postContent, (error, data) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(data);
-            });
-    });
-
-    return result;
 }
 
 export function filter(db, search) {
     console.log('filter()');
 
-    const url = `api/data_api/filter/${db}`;
-    const postContent = JSON.stringify({
+    return promisifyPost(`api/data_api/filter/${db}`, {
         searchString: search
-    });
-
-    return new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(postContent, (error, data) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(data);
-            });
     });
 }
 
 export function query(db, search) {
     console.log('query()');
 
-    const url = `api/data_api/query/${db}`;
-    const postContent = JSON.stringify({
+    return promisifyPost(`api/data_api/query/${db}`, {
         searchString: search
-    });
-
-    return new Promise((resolve, reject) => {
-        json(url)
-            .header('Content-Type', 'application/json')
-            .post(postContent, (error, data) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                resolve(data);
-            });
     });
 }
